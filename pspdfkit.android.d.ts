a7510ecb65c6fd5227de2e452ae3ea311d02bef6
Sean Perkins <sean@meetmaestro.com>
2016-08-03 21:03:07 -0400

    Initial commit

---
+++ pspdfkit.android.d.ts
@@ -0,0 +1,3 @@
+import { Common } from './pspdfkit.common';
+export declare class PSPDFKit extends Common {
+}
---
+++ pspdfkit.ios.ts
@@ -0,0 +1,5 @@
+import {Common} from './pspdfkit.common';
+
+export class PSPDFKit extends Common {
+
+}
