6d368cb6d08702c6dd9cfb66accd6b551eea9f17
Sean Perkins <sean@meetmaestro.com>
2016-08-04 17:32:41 -0400

    post install script re-work

--- scripts/install.js
+++ scripts/install.js
@@ -3,44 +3,96 @@
 var fs = require('fs');
 var prompt = require('prompt');
 
+var directory = {
+    ios: './platforms/ios',
+    android: './platforms/android'
+};
+
 console.log('NativeScript PSPDFKit Plugin Installation');
 
 prompt.start();
 
-prompt.get([{
-    name: 'ios_token',
-    description: 'Enter iOS POD token',
-    required: true
-}, {
-    name: 'android_token',
-    description: 'Enter android gradle token',
-    required: true
-}, {
-    name: 'android_license',
-    description: '(Optional) Enter Android license key',
-    required: false
-}], function(err, result) {
+prompt.get({
+    name: 'using_ios',
+    description: 'Are you using iOS (y/n)',
+    default: 'Y'
+}, function(err, result) {
     if(err) {
         return console.log(err);
     }
-    if(result.ios_token) {
-        writeiOSPOD(result.ios_token.trim());
+    if(result.using_ios && result.using_ios.toLowerCase() === 'y') {
+        promptiOSDialog();
     }
-    if(result.android_token) {
-        writeAndroidGradle(result.android_token.trim(), result.android_license);
+    else {
+        usingAndroidPrompt();
     }
 });
 
-function writeiOSPOD(token) {
-    fs.writeFile('./platforms/ios/Podfile', 'pod \'PSPDFKit\', podspec: \'https://customers.pspdfkit.com/cocoapods/' + token + '/latest.podspec\'', function(err) {
+function usingAndroidPrompt() {
+    prompt.get({
+        name: 'using_android',
+        description: 'Are you using Android (y/n)',
+        default: 'Y'
+    }, function(err, result) {
         if(err) {
             return console.log(err);
         }
-        console.log('PSPDFKit iOS Podfile generated successfully.');
+        if(result.using_android && result.using_android.toLowerCase() === 'y') {
+            promptAndroidDialog();
+        }
+    });
+}
+
+function promptAndroidDialog() {
+    prompt.get([{
+        name: 'android_token',
+        description: 'Enter android gradle token',
+        required: true
+    }, {
+        name: 'android_license',
+        description: '(Optional) Enter Android license key',
+        required: false
+    }], function(err, result) {
+        if(err) {
+            return console.log(err);
+        }
+        if(result.android_token) {
+            writeAndroidGradle(result.android_token.trim(), result.android_license);
+        }
+    });
+}
+
+function promptiOSDialog() {
+    prompt.get({
+        name: 'ios_token',
+        description: 'Enter iOS POD token',
+        required: true
+    }, function(err, result) {
+        if(err) {
+            return console.log(err);
+        }
+        if(result.ios_token) {
+            writeiOSPOD(result.ios_token);
+        }
+        usingAndroidPrompt();
+    });
+}
+
+function writeiOSPOD(token) {
+    if(!fs.existsSync(directory.ios)) {
+        fs.mkdirSync(directory.ios);
+    }
+    fs.writeFile(directory.ios + '/Podfile', 'pod \'PSPDFKit\', podspec: \'https://customers.pspdfkit.com/cocoapods/' + token + '/latest.podspec\'', function(err) {
+        if(err) {
+            return console.log(err);
+        }
     });
 }
 function writeAndroidGradle(token, license) {
-    fs.writeFile('./platforms/android/include.gradle', `android {
+    if(!fs.existsSync(directory.android)) {
+        fs.mkdirSync(directory.android);
+    }
+    fs.writeFile(directory.android + '/include.gradle', `android {
         productFlavors {
             "nativescript-pspdfkit" {
                 dimension "nativescript-pspdfkit"
