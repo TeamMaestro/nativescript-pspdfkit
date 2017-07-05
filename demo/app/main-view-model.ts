40500b62644c6930be1e2144f266f245d6293460
Osei Fortune <fortune.osei@yahoo.com>
2017-07-05 15:48:12 -0400

    Demo update

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,6 +1,7 @@
-import {Observable} from 'data/observable';
-import {TNSPSPDFKit} from 'nativescript-pspdfkit';
-
+import { Observable } from 'data/observable';
+import { TNSPSPDFKit } from 'nativescript-pspdfkit';
+import * as frame from 'ui/frame';
+import * as dialogs from 'ui/dialogs';
 var app = require('application');
 
 export class HelloWorldModel extends Observable {
@@ -11,15 +12,27 @@ export class HelloWorldModel extends Observable {
     constructor() {
         super();
 
-        if(app.ios) {
-            this.licenseKey = "Zw/2zlrEdeA/A43T8SPR8bYVwMZ1GgXc3RzCRTSuTc91YpqDNcMcIc5hCG/OD3Gsok9yjfSf0BEx8QRnPpk59XDUIwhAK/ye3/abUyexgqG1QS+JVd4sf5X+8UOeqg1PTLIEJa8CS3LAUQneC8anyL/QVY+IG0ODnSIoXr+8cthEr/Sa4CGtifs1VP+hFCQMS+4r0AKuwXPEOcBPGdsXSXOIMe9XSqNRDrUVoc7g9/8Sw4V4aykmQdrkem/tsvvvm0SbEcUSNMs1F0QpOiM9DEGw4G/83TyjKNkh+446z50dG3u7j+JWhPv6MP7AmQ3lQOAV6Svh5oL0sQ1dpzNk2UmhrUqJ7hZGPJXPO4imGqcquGfH6uvoTgSUMZVp+Auck/EkkAbKD6yeFGko8VAi968v2PhcRX65Ud/O6jIV2AjfqbrHqxxxlzwdnD2vwuAq";
+        if (app.ios) {
+            this.licenseKey = "lXkjKsBjph1SshSQfeKJlR1ngxiPpeNSPSu8Vy7PcLX60JtMPKNEorwPK3mh5taLznj7wdXLlZ3prGhGjOpNGK3tiHK7bsXRxkAC1j5vzLhyBNwPV07d39AUEVtmLNVbvd5Br/J5DhaazrjJKHTsNCivCRKuPKUAWo18ebe/Li6eEOotAVO9nNINbCAAo9EmaXHRE4rLnclWiYCJ6l6SBdGR/oYSzo/gTWDIJY4zkrewmqCHY/EKS6ZL0Vt57eFxsMfZusqZiTTFE/g4QUwt5/u2WjDpABqHeA3Ox4V2D/4Qgy03soaEkXGUgSmwsXQ17Rv4Ho+2UJvXzWfi/aYUlfrAH+skYI3QHpBWMkbJgEiUTyzuEv2eCzKq2U5SSIkOlWLMi7iPZ4ky3V53lz9/K0lgE7X0VqqMzabbZdyCFrR7UlgI0JROD2aq3RTKv5LJ";
         }
-        if(app.android) {
-            this.licenseKey = 'd04Z1gawu-vIGgDOI59iXoJ8_cAj5YIzybrgDFcxelVd5Xa95tPE38TeUdE3Y0PjYUrtUc2lv71sSJdtQt7xsf4o-aTXpUDjpyK-HYrSDJ7kb-NoX5xYAL9I_Dcg2IrA2FiMDWc-pS7530C4uOyHzV06UlFYvfC4j35_C3r1j-VjRyKm5nLxEvyk7O26voFFoDQ9MdCDVnc7yAsmr1VT4Pk4Xn4usHuj0QKTm4lIURu_OurwljbaZaGNG4rr3b9rxXTLbxBbHsznqB2NfIY3xdpxEXiWtykacX7-voQikzfzIdWUPogwNywPIYvTwCZNaY6n2Hv2Fn5jLhT5iljHhxVGz9XAFbS2ro8bwE6tgoWYKPAVLbJI04qWSz18Wm7s-rqua7f0D2-WnnosdHSGV58IvI1bFEZgA7UDZqmF7cLlfz0ax1ZcP0wC-cTCHpKX';
+        if (app.android) {
+            this.licenseKey = 'WH434PCsmv_l7sCKpXfhRMownUPWmVibyIwk6MXNaBA0iRv_iACfquzNgm-qJcjgECglc-J-bfwS91t4_2sxWcpHKOSfGIAFhjNK3T9CQHnWftVNPdLpfGYwXamZinu_2m3uxciKsGce8OJ43O3Q7SbvYmVqGeWwRDLe5ouOgHXFwlDYmjYA5nX9fNL5Ex_myyd72sPoLpgkeUjQIuEA7RFO-rQ0DmozKd9N-LC65dbiwuuLfbdjKf0_ctuRVpKuPjLe9QICdrVHVQFfjhj9VQNnzFIVkZq7fJjhQ6qC_KaqiZlKzGsx13Cwssi3WzQf_uAQBzm4Ty6RX8LJ4FyiELxwvcsy_vUgoY61P2KuOce9s6KgJ4M9tpFsa0-oZBrbuvOnkK0vGovwb1kvr5Wb6iRGyLhhQzAuwztCyNPh-la-RQ0l277asDhM4HlCyhAX';
         }
 
         this.pspdfkit = new TNSPSPDFKit(this.licenseKey);
-       // this.pspdfkit.display('~/example.pdf');
     }
 
+    getName() {
+        const pdf: any = frame.topmost().getViewById('pdf');
+        dialogs.alert(pdf.getFormField('Given Name Text Box'));
+    }
+
+    fillForm() {
+        const pdf: any = frame.topmost().getViewById('pdf');
+        pdf.setFormFields({ "Given Name Text Box": { "value": "Osei" }, "Family Name Text Box": { "value": "Fortune" }, "House nr Text Box": { "value": "3" }, "Address 2 Text Box": { "value": "" }, "Postcode Text Box": { "value": "000000" }, "Country Combo Box": { "value": "Trinidad & Tobago" }, "Height Formatted Field": { "value": "180" }, "City Text Box": { "value": "Siparia" }, "Driving License CheckBox": { "value": "Yes" }, "Favourite Colour List Box": { "value": "White" }, "Language 1 Check Box": { "value": "Yes" }, "Language 2 Check Box": { "value": "Yes" }, "Language 3 Check Box": { "value": "Yes" }, "Language 4 Check Box": { "value": "Yes" }, "Language 5 Check Box": { "value": "Yes" }, "Gender List Box": { "value": "Woman" }, "Address 1 Text Box": { "value": "Happy Hill Quinam Road" } });
+    }
+    clearForm() {
+        const pdf: any = frame.topmost().getViewById('pdf');
+        pdf.setFormFields({ "Given Name Text Box": { "value": "" }, "Family Name Text Box": { "value": "" }, "House nr Text Box": { "value": "" }, "Address 2 Text Box": { "value": "" }, "Postcode Text Box": { "value": "" }, "Country Combo Box": { "value": "" }, "Height Formatted Field": { "value": "" }, "City Text Box": { "value": "" }, "Driving License CheckBox": { "value": "" }, "Favourite Colour List Box": { "value": "" }, "Language 1 Check Box": { "value": "" }, "Language 2 Check Box": { "value": "" }, "Language 3 Check Box": { "value": "" }, "Language 4 Check Box": { "value": "" }, "Language 5 Check Box": { "value": "" }, "Gender List Box": { "value": "" }, "Address 1 Text Box": { "value": "" } })
+    }
 }
