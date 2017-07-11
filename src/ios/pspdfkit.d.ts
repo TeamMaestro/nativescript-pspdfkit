d6875417d56f5c0b073f287e2ac58759f8e570cd
Osei Fortune <fortune.osei@yahoo.com>
2017-07-11 11:32:15 -0400

    Update

--- src/ios/pspdfkit.d.ts
+++ src/ios/pspdfkit.d.ts
@@ -15,6 +15,13 @@ export declare class TNSPSPDFView extends View {
     initNativeView(): void;
     disposeNativeView(): void;
     onMeasure(widthMeasureSpec: number, heightMeasureSpec: number): void;
+    backgroundColor: string;
+    getAnnotationField(name: string): any;
+    getFormField(name: string): any;
+    getAllFormFields(): Object;
+    setFormField(name: string, value: any): void;
+    setFormFields(obj: Object): void;
+    formsEnabled: boolean;
     scrollDirection: string;
     fitToWidth: boolean;
     thumbnailBar: string;
